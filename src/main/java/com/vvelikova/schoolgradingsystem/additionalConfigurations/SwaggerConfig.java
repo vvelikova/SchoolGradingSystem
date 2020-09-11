2def6c42bfeee2fdc80537278d2e5131d418db30
vvelikova <vela.velikova@gmail.com>
2020-09-11 18:46:21 +0300

    Dependencies for swagger added; Swagger config file added

---
+++ src/main/java/com/vvelikova/schoolgradingsystem/additionalConfigurations/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.vvelikova.schoolgradingsystem.additionalConfigurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2 // enables Swagger support in the class
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vvelikova.schoolgradingsystem"))
+                .paths(regex("/api.*"))
+                .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addResourceHandlers( ResourceHandlerRegistry registry )
+            {
+                registry.addResourceHandler( "swagger-ui.html" ).addResourceLocations( "classpath:/META-INF/resources/" );
+                registry.addResourceHandler( "/webjars/**" ).addResourceLocations( "classpath:/META-INF/resources/webjars/" );
+            }
+        };
+    }
+}
