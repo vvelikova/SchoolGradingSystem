4b3ee95d53bb12167e9c449aab604608d0f4940c
vvelikova <vela.velikova@gmail.com>
2020-09-13 11:09:45 +0300

    exceptions for Mark added

---
+++ src/main/java/com/vvelikova/schoolgradingsystem/exceptions/MarkNotFoundExceptoin.java
@@ -0,0 +1,7 @@
+package com.vvelikova.schoolgradingsystem.exceptions;
+
+public class MarkNotFoundExceptoin extends RuntimeException {
+    public MarkNotFoundExceptoin(String message) {
+        super(message);
+    }
+}
