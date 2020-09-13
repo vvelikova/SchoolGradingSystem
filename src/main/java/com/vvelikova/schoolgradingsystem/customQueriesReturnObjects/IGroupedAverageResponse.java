153216d450f08a261f2ad73fbf5deec16fb623b5
vvelikova <vela.velikova@gmail.com>
2020-09-14 01:21:41 +0300

    creating dedicated return type for endpoint returning data for average mark for all existing combinations of a student and a course / Projection

---
+++ src/main/java/com/vvelikova/schoolgradingsystem/customQueriesReturnObjects/IGroupedAverageResponse.java
@@ -0,0 +1,7 @@
+package com.vvelikova.schoolgradingsystem.customQueriesReturnObjects;
+
+public interface IGroupedAverageResponse {
+    String getStudentName();
+    String getCourseName();
+    Double getAverageMark();
+}
