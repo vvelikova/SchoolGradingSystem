be1bde76cf09cd51800ac45748228a31595aa65a
vvelikova <vela.velikova@gmail.com>
2020-09-14 22:10:53 +0300

    further logic added when updating a course

--- src/main/java/com/vvelikova/schoolgradingsystem/services/CourseService.java
+++ src/main/java/com/vvelikova/schoolgradingsystem/services/CourseService.java
@@ -1,11 +1,17 @@
 package com.vvelikova.schoolgradingsystem.services;
 
 import com.vvelikova.schoolgradingsystem.domain.Course;
+import com.vvelikova.schoolgradingsystem.domain.Mark;
 import com.vvelikova.schoolgradingsystem.exceptions.CourseNotFoundException;
 import com.vvelikova.schoolgradingsystem.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class CourseService {
@@ -13,6 +19,13 @@ public class CourseService {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    @Lazy
+    private MarkService markService;
+
+    /** saveUpdateCourse() handles the update ONLY of the name of the Course.
+     *  If there is a need for a certain mark to be changed, the methods exposed by the MarkService should be used.
+     *  saveUpdateCourse() handles the update of the linked marks a Course has */
     public Course saveUpdateCourse(Course theCourse) {
 
         if (theCourse.getId() != null) {
@@ -21,6 +34,18 @@ public class CourseService {
             if (existingCourse == null) {
                 throw new CourseNotFoundException("Course with ID: " + theCourse.getId() + " does NOT exist in the system.");
             }
+
+            List<Mark> courseMarks = existingCourse.getMarks();
+            List<Mark> updatedMarkInfo = new ArrayList<>();
+
+            for (Mark obj: courseMarks) {
+                Mark tempMark = markService.getMarkById(obj.getId());
+                tempMark.setCourseName(theCourse.getCourseName());
+                tempMark.setCourse(theCourse);
+                tempMark.setMark_date(new Date());
+                updatedMarkInfo.add(tempMark);
+            }
+            theCourse.setMarks(updatedMarkInfo);
         }
 
         return courseRepository.save(theCourse);
